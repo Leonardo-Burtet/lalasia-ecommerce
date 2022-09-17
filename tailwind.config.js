19e7425f280275548c11af865be8d2299c062286
leonardoburtet <leonardoburtet@outlook.com>
2022-09-17 17:57:13 -0300

    corrigir tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  theme: {
+    colors: {
+      'brown-500': '#AD7E5C',
+
+      'orange-200': '#FFF0D9',
+      'orange-300': '#FFE5BF',
+      'orange-400': '#FFD89F',
+      'orange-500': '#FFCC7F',
+      'orange-600': '#FFBF5F',
+      'orange-700': '#FFB23F',
+
+      'green-dark-200': '#DCE7E6',
+      'green-dark-300': '#C5D6D5',
+      'green-dark-400': '#A8C2C0',
+      'green-dark-500': '#8BAEAB',
+      'green-dark-600': '#6E9996',
+      'green-dark-700': '#518581',
+
+      'white-500': '#F9F9F9',
+      'screen-gray': '#F3F3F3',
+      'screen-white': '#FFFFFF'
+    },
+    textColor: {
+      black: '#151411',
+      paragraph: '#AFADB5'
+    },
+    extend: {}
+  },
+  plugins: []
+}
